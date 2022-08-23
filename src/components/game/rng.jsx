9c67813fe6bb36e0c30b5216d8aafcbffe7a5e46
Mateusz Myśliwiec <mateuszmysliwiec03@gmail.com>
2022-08-23 22:06:16 +0200

    Bug

--- src/components/game/rng.jsx
+++ src/components/game/rng.jsx
@@ -1,10 +1,14 @@
 const Rng = () => {
   let val1, val2;
 
-  do {
+  while (!val1 && !val2) {
     val1 = Math.floor(Math.random() * 100) + 1;
     val2 = Math.floor(Math.random() * 100) + 1;
-  } while (!val1 && val2 > 100 && val1 > 100 && val2 - 10 === val1);
+    if (val1 === val2) {
+      val1 = null;
+      val2 = null;
+    }
+  }
 
   return { first: val1, second: val2 };
 };
